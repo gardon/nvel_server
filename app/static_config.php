de9c2ad430e197f46267bced9be7c16eb089f9c8
Franz Vanderlinde <franz.vanderlinde@ffwagency.com>
2018-02-16 17:26:43 -0200

    Directs bots and spiders to backend to get dynamic yet server-side content.

---
+++ app/static_config.php
@@ -0,0 +1,7 @@
+<?php
+
+function static_config() {
+  return array(
+    'backend_url' => 'http://server.nvel.docksal'
+  );
+}
---
+++ app/static_get.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once "static_config.php";
+$config = static_config();
+
+$path = explode('/', $_GET['q']);
+switch ($path[0]) {
+  case 'index':
+  case 'about':
+    $url = $config['backend_url'];
+    break;
+  case 'chapters':
+    if (count($path) == 1) {
+      $url = $config['backend_url'];
+      break;
+    }
+    elseif (count($path) == 2 && is_numeric($path[1])) {
+      $url = $config['backend_url'] . '/node/' . $path[1];
+      break;
+    }
+    // Intentionally fallback to default fom here
+  default:
+    header("HTTP/1.0 404 Not Found", true, 404);
+    die();
+}
+
+redirect($url);
+
+function redirect($url, $statusCode = 303) {
+  header('Location: ' . $url, true, $statusCode);
+  die();
+}
